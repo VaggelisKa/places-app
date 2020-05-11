9b6f043cf36c8cb04a717b8ec8435aebc18b390d
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-11 18:10:13 +0300

    Extracting the loaded places id

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Place } from '../../models/place.model';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-bookings.page.scss'],
 })
 export class OfferBookingsPage implements OnInit {
+  place: Place;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _navController: NavController,
+              private _placesService: PlacesService) { }
 
   ngOnInit() {
+    this._route.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('placeId')) {
+        this._navController.navigateBack('/places/tabs/offers');
+      }
+
+      this.place = this._placesService.getPlaces().find(p => p.id === paramMap.get('placeId'));
+    });
   }
 
 }
