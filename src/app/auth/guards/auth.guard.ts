2b9b2cca73fc9cb0c7643a587b0b788cad9155bc
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-13 20:18:08 +0300

    Changing the auth guard's return value

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -11,14 +11,18 @@ import { getIsAuth } from '../auth-store/auth.selectors';
     providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+    isAuth: boolean;
     constructor(private _authService: AuthService,
                 private _router: Router,
                 private _store: Store<fromAuth.State>) {}
 
     canLoad(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> | Promise<boolean> {
-        if (!this._store.pipe(select(getIsAuth))) {
+        this._store.pipe(select(getIsAuth)).subscribe(result => {
+            this.isAuth = result;
+        });
+        if (!this.isAuth) {
             this._router.navigate(['/auth']);
         }
-        return  this._store.pipe(select(getIsAuth));
+        return  this.isAuth;
     }
 }
