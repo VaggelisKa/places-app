6b441b3cbb62f109d2f5439c3bb1daeaa6ed25d3
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-13 21:31:32 +0300

    Replacing spinner with loading controller

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { Store, select } from '@ngrx/store';
 import * as fromAuth from './auth-store/auth.reducer';
 import * as AuthSelectors from './auth-store/auth.selectors';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-auth',
@@ -17,15 +18,23 @@ export class AuthPage implements OnInit {
 
   constructor(private _authService: AuthService,
               private _router: Router,
-              private _store: Store<fromAuth.State>) { }
+              private _store: Store<fromAuth.State>,
+              private _loadingController: LoadingController) { }
 
   ngOnInit(): void {
     this.isAuth$ = this._store.pipe(select(AuthSelectors.getIsAuth));
     this.isLoading$ = this._store.pipe(select(AuthSelectors.authLoading));
   }
 
-  onLogin(): void {
+  async onLogin() {
     this._authService.login();
+
+    const loading = await this._loadingController.create({
+      message: 'Logging in...',
+      spinner: 'crescent',
+      duration: 2000
+    });
+    await loading.present();
   }
 
 }
