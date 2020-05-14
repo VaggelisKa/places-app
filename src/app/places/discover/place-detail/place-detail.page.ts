cd42ee761bc220fe83a373b4695014798160ac45
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-14 14:30:55 +0300

    Adding action sheet controller

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../models/place.model';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
@@ -16,7 +16,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(private _route: ActivatedRoute,
               private _placesService: PlacesService,
               private _navController: NavController,
-              private _modalController: ModalController) { }
+              private _modalController: ModalController,
+              private _actionSheetController: ActionSheetController) { }
 
   ngOnInit() {
     this._route.paramMap.subscribe(paramMap => {
@@ -28,7 +29,37 @@ export class PlaceDetailPage implements OnInit {
     });
   }
 
-  async onBookPlace(): Promise<{}> {
+  async onBookPlace() {
+    const actionSheet = await this._actionSheetController.create({
+      header: 'Choose an action...',
+      buttons: [{
+        text: 'Cancel',
+        role: 'cancel',
+        icon: 'close'
+      },
+
+      {
+        text: 'Select a date',
+        icon: 'today-outline',
+        handler: () => {
+          this.openModal('select');
+        }
+      },
+
+      {
+        text: 'Choose random date',
+        icon: 'help-circle-outline',
+        handler: () => {
+          this.openModal('random');
+        }
+      }
+    ]
+    });
+    await actionSheet.present();
+
+  }
+
+  async openModal(mode: 'select' | 'random'): Promise<{}> {
     const modal = await this._modalController.create({
       component: CreateBookingComponent,
       componentProps: {selectedPlace: this.place},
