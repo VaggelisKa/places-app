05c67378d97f730d5b07218544a5afe60b6e56bf
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-15 18:23:54 +0300

    Adding custom password match validator

--- src/app/auth/signup/signup.page.ts
+++ src/app/auth/signup/signup.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
 import { Observable } from 'rxjs';
@@ -8,6 +8,7 @@ import * as fromAuth from '../auth-store/auth.reducer';
 import * as AuthSelectors from '../auth-store/auth.selectors';
 import { Store, select } from '@ngrx/store';
 import { AuthService } from '../services/auth.service';
+import { ConfirmedValidator } from './confirm-passwords.validator';
 
 @Component({
   selector: 'app-signup',
@@ -22,17 +23,24 @@ export class SignupPage implements OnInit {
   constructor(private _router: Router,
               private _loadingController: LoadingController,
               private _store: Store<fromAuth.State>,
-              private _authService: AuthService) { }
+              private _authService: AuthService,
+              private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.signupForm = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]),
-      'password': new FormControl(null, Validators.required),
-      'confirmPassword': new FormControl(null, Validators.required)
+    this.signupForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)]],
+      password: ['', Validators.required],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validator: ConfirmedValidator('password' , 'confirmPassword')
     });
 
     this.isAuth$ = this._store.pipe(select(AuthSelectors.getIsAuth));
     this.isLoading$ = this._store.pipe(select(AuthSelectors.authLoading));
+  }
+
+  get f() {
+    return this.signupForm.controls;
   }
 
   onSwitchToLogin() {
@@ -42,7 +50,7 @@ export class SignupPage implements OnInit {
   async onSignup() {
     this._authService.signup();
 
-    console.log(this.signupForm);
+    console.log(this.signupForm.value);
     const loading = await this._loadingController.create({
       message: 'Creating Account...',
       spinner: 'crescent',
