d4aef8e1fc075d3fb500357991c972603ca32cbb
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-19 15:27:59 +0300

    Adding spinner when loading (needs visual improvement)!

--- src/app/places/offers/offer-item/offer-item.component.ts
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Place } from '../../models/place.model';
 
+import { Store, select } from '@ngrx/store';
+import * as fromPlaces from '../../placesStore/places.reducer';
+import * as placesSelectors from '../../placesStore/places.selectors';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-offer-item',
   templateUrl: './offer-item.component.html',
@@ -8,10 +13,13 @@ import { Place } from '../../models/place.model';
 })
 export class OfferItemComponent implements OnInit {
   @Input() offer: Place;
+  isLoading$: Observable<boolean>;
 
-  constructor() { }
+  constructor(private _store: Store<fromPlaces.State>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isLoading$ = this._store.pipe(select(placesSelectors.placesLoading));
+  }
 
   getDummyDate(): Date {
     return new Date();
