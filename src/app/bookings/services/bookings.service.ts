92f7bc18618664c422bde40f98fcf64b2ce1cff2
VaggelisKa <e.karavasileiadis@gmail.com>
2020-06-05 13:25:52 +0300

    Deleting unused bookings array

--- src/app/bookings/services/bookings.service.ts
+++ src/app/bookings/services/bookings.service.ts
@@ -3,11 +3,8 @@ import { Booking } from '../models/booking.model';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 
-import { Store } from '@ngrx/store';
-import * as fromBookings from '../bookings-store/bookings.reducer';
-import * as BookingsActions from '../bookings-store/bookings.actions';
-import { catchError, tap, map } from 'rxjs/operators';
-import { throwError, pipe } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 interface BookingData {
     dateFrom: string;
@@ -24,34 +21,7 @@ interface BookingData {
 export class BookingsService {
     private readonly path = environment.firebaseUrl + 'bookings';
 
-    constructor(private _store: Store<fromBookings.State>,
-                private _http: HttpClient) {}
-
-    private bookings: Booking[] = [
-        {
-            id: 'ohafhusdoad',
-            placeId: 'p1',
-            userId: 'ioijfdjkfdj',
-            placeTitle: 'New york mansion',
-            firstName: 'Vasilis',
-            lastName: 'karas',
-            guestNumber: 7,
-            dateFrom: new Date('2020-10-04'),
-            dateTo: new Date('2020-10-10')
-        },
-
-        {
-            id: 'oopsaospaosp',
-            placeId: 'p2',
-            userId: 'ioijfdjkfdsasaj',
-            placeTitle: 'Greek bangalow',
-            firstName: 'Nikos',
-            lastName: 'Olatunji',
-            guestNumber: 3,
-            dateFrom: new Date('2020-10-04'),
-            dateTo: new Date('2020-10-10')
-        }
-    ];
+    constructor(private _http: HttpClient) {}
 
     fetchBookings() {
         return this._http
